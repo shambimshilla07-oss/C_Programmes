3f9a86b86f4d0dd4af8ecd43c9df22a0f4978fea
cool kheed jay <shambimshilla07@gmail.com>
2025-09-19 14:14:46 +0300

    Add files via upload

---
+++ Introduction to programming.c
@@ -0,0 +1,27 @@
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ //units for this sem
+ 
+ #include<stdio.h>
+ 
+ int main() {
+ 
+ 
+ printf("Communication skills\n");
+ printf("Physics\n");
+ printf("Analogue electronics\n");
+  printf("Introduction to computer architecture\n");
+  printf("fundamentals of ICT\n");
+  printf("Discrete mathematics\n");
+  printf("Mathematics for science\n");
+  printf("Introductionto programming and problem solving\n");
+  
+  
+  }
