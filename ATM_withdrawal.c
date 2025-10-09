caf87fa97450028273e4dc89a1c795ad283576ab
cool kheed jay <shambimshilla07@gmail.com>
2025-10-09 08:49:26 +0300

    Add files via upload

---
+++ ATM_withdrawal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main () {
+
+
+//initial balance example
+int balance =10000;
+int withdrawal;
+
+while(balance>0) {
+
+printf("Enter amount to withdraw");
+scanf("%d", &withdrawal);
+
+balance -=withdrawal;
+printf("Remaining balance: %d\n",balance);
+}
+
+printf("Account balance is zero or negative\n");
+return 0;
+
+}
